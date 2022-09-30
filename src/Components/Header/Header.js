2037c2ae801f834248761dc88e73403eca2c5bc7
Minnatulislam <minnatulislam86@gmail.com>
2022-09-30 19:03:12 +0600

    added header sectionF

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+// import logo from "../../logo.png";
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="flex items-center">
+        {/* <img className="w-12 mr-2" src={logo.png} alt="" /> */}
+        <h1 className="text-3xl font-bold">MinnsPort Body Building</h1>
+      </div>
+        </div>
+    );
+};
+
+export default Header;
